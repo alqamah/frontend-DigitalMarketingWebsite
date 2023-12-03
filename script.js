1766dcc7099363673b5071a2c26790aba68c731f
Alqama Hasnain <alqama.hasnain@gmail.com>
2023-12-03 17:25:27 +0530

    hamburger menu

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ function render() {
         newElement.href = './style-dm.css';
     document.head.appendChild(newElement);
 }
+
+var hbtn = document.getElementById("hbgr-btn");
+
+window.console.log(hbtn); 
+
+hbtn.addEventListener('click', function () {
+    togglemenuview();
+    
+})
+
+var vflag = false;
+
+function togglemenuview(){
+    var menu = document.getElementById("nav");
+    var htxt = document.getElementById("main-text");
+    if (vflag==false){
+        menu.style.right = '0px';
+        htxt.style.marginTop = '130px';
+    }
+    else{
+        menu.style.right = '-100%';
+        htxt.style.marginTop = '20px';
+    }
+    vflag = !vflag;
+}
+
